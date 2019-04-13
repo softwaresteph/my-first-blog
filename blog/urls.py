608a4ecfed3efd81e28ededd8989fc90143bb97f
Steph Carter <beyondtheteal@gmail.com>
2019-04-12 23:59:21 -0400

    Added view and template for individual blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [ 
     path('', views.post_list, name="post_list"), # The empty quotation marks means that this will be the default route
-    
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
